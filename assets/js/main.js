d40b5aa006f154ee454c77db7fe5f8daeef7b414
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-03-29 15:05:16 +0200

    Upload Files

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+/*
+    Esercizio:
+    Data una lista della spesa, stampare sulla pagina gli elementi della lista individualmente con un ciclo while.
+    Consigli:
+    - Ricorda di inizializzare la variabile di contatore prima dell'inizio del ciclo while
+    - Ricorda di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
+*/ 
+
+let listaSpesa = [
+    'mele',
+    'insalta',
+    'patate',
+    'cipolla',
+    'mozzarella',
+    'carote',
+    'uova',
+    'pane',
+    'latte',
+    'pasta'
+]
+
+console.log(listaSpesa)
